96b1354ea2b1d4fa23afca1aec0c183afcb8841d
olegcntu <oleg.krasavcik.41@gmail.com>
2022-12-27 22:46:52 +0200

    with-security-and-users

---
+++ src/main/java/com/example/marketplace/controllers/UserController.java
@@ -0,0 +1,41 @@
+package com.example.marketplace.controllers;
+
+import com.example.marketplace.models.User;
+import com.example.marketplace.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+
+    @PostMapping("/registration")
+    public String createUser(User user, Model model) {
+        if (!userService.createUser(user)) {
+            model.addAttribute("errorMessage", "User with E-mail: " +
+                    user.getEmail() + " already exists");
+            return "redirect:/login";
+        }
+        return "redirect:/login";
+    }
+
+    @GetMapping("/hello")
+    public String securityUrl() {
+        return "hello";
+    }
+}
---
+++ src/main/java/com/example/marketplace/services/UserService.java
@@ -0,0 +1,31 @@
+package com.example.marketplace.services;
+
+import com.example.marketplace.models.User;
+import com.example.marketplace.models.enums.Role;
+import com.example.marketplace.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.sql.Struct;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public boolean createUser(User user) {
+        String email = user.getEmail();
+        if (userRepository.findByEmail(email) != null) return false;
+        user.setActive(true);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.getRoles().add(Role.ROLE_USER);
+        log.info("Saving new User with E-mail: {}", email);
+        userRepository.save(user);
+        return true;
+    }
+}
