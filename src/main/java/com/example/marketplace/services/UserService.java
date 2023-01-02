6b6a2d684b536f36f83318fb8f63819b6284f49f
olegcntu <oleg.krasavcik.41@gmail.com>
2023-01-02 22:08:35 +0200

    with GUI

--- src/main/java/com/example/marketplace/services/UserService.java
+++ src/main/java/com/example/marketplace/services/UserService.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.sql.Struct;
 import java.util.Arrays;
 import java.util.List;
@@ -67,4 +68,8 @@ public class UserService {
         userRepository.save(user);
     }
 
+    public User getUserByPrincipal(Principal principal) {
+        if (principal == null) return new User();
+        return userRepository.findByEmail(principal.getName());
+    }
 }
